bbc5cd0e690258de04a50ba78e4acb674bc0489f
momenazhar <momenazher57@gmail.com>
2023-10-11 08:59:42 +0300

    arrays

--- week5/index.php
+++ week5/index.php
@@ -5,20 +5,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <style>
-        * {
-            font-size: 2rem;
-        }
-    </style>
-    <?php
-        $x = 8;
-        $y = 36;
+    <h1>Indexed Array</h1>
+    <div class="container-fluid outer">
+        <?php
+            $colors = array("White", "Black", "Red", "Green", "Blue", "Purple", "Pink", "Orange", "Yellow", "aaa", "aa");
 
-        echo $x > 5 && $y > 5 ? "true" : "false";
-        echo "<br>";
-        echo $z ?? "does not exist";
-    ?>
+            for($x = 0; $x < count($colors); $x++) {
+                echo "<div class='inner'>".$colors[$x]."</div>";
+            }
+        ?>
+    </div>
+    <hr>
+    <h1>Associative Array</h1>
+    <div class="container-fluid outer">
+        <?php
+            $age = array("Deo" => "35", "Marco" => "22", "Sarah" => "20", "Isa" => "21", "John" => "19");
+
+            foreach($age as $x => $ageValue) {
+                echo "<div class='inner'> Key: ".$x."<br> Age: ".$ageValue."</div>";
+            }
+        ?>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
