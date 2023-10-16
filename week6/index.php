2c83977b43db9291cf371e3ea70b98eeffb96ad3
momenazhar <momenazher57@gmail.com>
2023-10-16 09:16:43 +0300

    get

--- week6/index.php
+++ week6/index.php
@@ -35,10 +35,10 @@
         <a href="index.php?course=Web&week=6" class="btn btn-primary">Submit</a>
         <hr>
         <?php
-        $course = $_GET['course'];
-        $week = $_GET['week'];
-        echo "<p>Course: <strong>".$course."</strong></p>";
-        echo "<p>Week: <strong>".$week."</strong></p>";
+            $course = $_GET['course'];
+            $week = $_GET['week'];
+            echo "<p>Course: <strong>".$course."</strong></p>";
+            echo "<p>Week: <strong>".$week."</strong></p>";
         ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
