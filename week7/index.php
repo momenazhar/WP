fb7f0a65726eb5d07bc48329f3ed4e181b40ecd9
momenazhar <momenazher57@gmail.com>
2023-10-23 09:30:45 +0300

    insert to mysql with form

--- week7/index.php
+++ week7/index.php
@@ -38,6 +38,39 @@
                 ?>
             </table>
         </div>
+        <div class="container">
+            <form action="index.php" method="get">
+                <input type="text" name="name" class="form-control nameInput">
+                <br>
+                <input type="email" name="email" class="form-control emailInput">
+                <br>
+                <select name="gender" class="form-control genderInput">
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </select>
+                <br>
+                <input type="date" name="dob" class="form-control dateInput">
+                <br>
+                <button class="btn btn-primary submission" type="submit">Insert Data</button>
+            </form>
+        </div>
+        <?php
+                if($_SERVER['REQUEST_METHOD'] == 'GET') {
+                    $name = $_GET['name'];
+                    $email = $_GET['email'];
+                    $gender = $_GET['gender'];
+                    $dob = $_GET['dob'];
+                if(!$name | !$email | !$gender | !$dob) {
+                } else {
+                    $insert = "INSERT INTO `student`(`name`, `email`, `gender`, `dob`) VALUES ('$name', '$email', '$gender', '$dob');";
+
+                    if(mysqli_query($conn, $insert)) {
+                    } else {
+                        mysqli_error($conn);
+                    }
+                }
+            }
+        ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
