8e0f232e6621df8c62ce538b4d9cfc67dde2527e
momenazhar <momenazher57@gmail.com>
2023-10-25 09:02:16 +0300

    delete btns

--- week7/index.php
+++ week7/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 </head>
 <body>
     <br>
@@ -28,13 +28,15 @@
                 <?php
                 if(mysqli_num_rows($result) > 0) {
                     while($row = mysqli_fetch_assoc($result)) {
+                        $id = $row['id'];
                         echo "<tr>".
                             "<td>".$row['id']."</td>".
                             "<td>".$row['name']."</td>".
                             "<td>".$row['email']."</td>".
                             "<td>".$row['gender']."</td>".
-                            "<td>".$row['dob']."</td>"
-                        ."</tr>";
+                            "<td>".$row['dob']."</td>".
+                            "<td><button id='$id'><i class='bi bi-trash-fill'/></button></td>".
+                        "</tr>";
                     }
                 } else {
                     echo "No records";
