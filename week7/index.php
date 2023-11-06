f72d7d30b4b96993c51be3bf685fad6ee8a1956b
momenazhar <momenazher57@gmail.com>
2023-11-06 08:58:32 +0300

    add modal

--- week7/index.php
+++ week7/index.php
@@ -52,24 +52,51 @@
             </table>
         </div>
         <div class="container">
-            <form action="insert.php" method="post">
-                <label for="name">Name</label>
-                <input type="text" name="name" class="form-control nameInput" placeholder="Insert Name">
-                <br>
-                <label for="email">Email</label>
-                <input type="email" name="email" class="form-control emailInput" placeholder="Insert Email">
-                <br>
-                <label for="gender">Gender</label>
-                <select name="gender" class="form-control genderInput">
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                </select>
-                <br>
-                <label for="dob">Date of Birth</label>
-                <input type="date" name="dob" class="form-control dateInput">
-                <br>
-                <button class="btn btn-primary submission" type="submit">Insert Data</button>
-            </form>
+
+        </div>
+
+
+        <div class="container">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addModal">
+                Add New Records
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="addModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <form action="insert.php" method="post">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="addModalLabel">Insert Data</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                        <label for="name">Name</label>
+                        <input type="text" name="name" class="form-control nameInput" placeholder="Insert Name">
+                        <br>
+                        <label for="email">Email</label>
+                        <input type="email" name="email" class="form-control emailInput" placeholder="Insert Email">
+                        <br>
+                        <label for="gender">Gender</label>
+                        <select name="gender" class="form-control genderInput">
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                        </select>
+                        <br>
+                        <label for="dob">Date of Birth</label>
+                        <input type="date" name="dob" class="form-control dateInput">
+                        <br>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button class="btn btn-primary" type="submit">Insert Data</button>
+                </div>
+                </form>
+                </div>
+            </div>
+            </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
