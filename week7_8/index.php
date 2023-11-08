2a0104e18c02cb8a4331722b6fc8d456eb5d7dbb
momenazhar <momenazher57@gmail.com>
2023-11-08 09:04:38 +0300

    styling

--- week7_8/index.php
+++ week7_8/index.php
@@ -19,29 +19,32 @@
         $result = mysqli_query($conn, $query);
         ?>
         <div class="container">
+            <h1 class="text-center">Student Database</h1>
+            <br>
             <table class="table table-striped table-hover table-bordered">
                 <tr>
-                    <th class='align-middle'>ID</th>
-                    <th class='align-middle'>Name</th>
-                    <th class='align-middle'>Email</th>
-                    <th class='align-middle'>Gender</th>
-                    <th class='align-middle'>Date of Birth</th>
+                    <th class='text-center'>ID</th>
+                    <th class='text-center'>Name</th>
+                    <th class='text-center'>Email</th>
+                    <th class='text-center'>Gender</th>
+                    <th class='text-center'>Date of Birth</th>
+                    <th class='text-center' colspan='2'>Action</th>
                 </tr>
                     <?php
                     if(mysqli_num_rows($result) > 0) {
                         while($row = mysqli_fetch_assoc($result)) {
                             $id = $row['id'];
                             echo "<tr>".
-                                "<td class='align-middle'>".$row['id']."</td>".
+                                "<td class='align-middle text-center' style='width: 42px;'>".$row['id']."</td>".
                                 "<td class='align-middle'>".$row['name']."</td>".
                                 "<td class='align-middle'>".$row['email']."</td>".
                                 "<td class='align-middle'>".$row['gender']."</td>".
                                 "<td class='align-middle'>".$row['dob']."</td>".
-                                "<form action='deleteInterface.php' method='GET'>".
-                                    "<td class='align-middle text-center' style='margin: 0 !important;'><button type='submit' name='id' value='$id' class='btn btn-danger'><i class='bi bi-trash-fill'/></button></td>".
-                                "</form>".
                                 "<form action='updateInterface.php' method='GET'>".
-                                    "<td class='align-middle text-center' style='margin: 0 !important;'><button type='submit' name='id' value='$id' class='btn btn-primary'><i class='bi bi-pencil-square' /></button></td>".
+                                    "<td class='align-middle text-center' style='margin: 0 !important; width: 38px;'><button type='submit' name='id' value='$id' class='btn btn-info' style='padding: 0 !important; height: 38px; width: 38px; display: flex; align-items: center; justify-content: center;'><svg xmlns='http://www.w3.org/2000/svg' fill='white' height='20' viewBox='0 -960 960 960' width='20'><path d='M206.783-100.782q-44.305 0-75.153-30.848-30.848-30.848-30.848-75.153v-546.434q0-44.305 30.848-75.153 30.848-30.848 75.153-30.848h356.435L457.216-753.217H206.783v546.434h546.434v-250.303l106.001-106.001v356.304q0 44.305-30.848 75.153-30.848 30.848-75.153 30.848H206.783ZM480-480Zm-129.044 76.043v-86.26q0-21.225 7.978-40.461 7.979-19.235 22.936-34.192l327.607-327.043q15.957-15.957 35.479-23.652 19.522-7.696 39.609-7.696 20.522 0 39.827 7.696 19.304 7.695 35.261 23.652l32.26 32.695q15.522 15.956 23.217 35.318 7.696 19.363 7.696 39.392 0 20.03-7.392 39.528-7.392 19.497-23.521 35.633L564.87-382.304q-14.957 14.956-34.192 23.152-19.236 8.196-40.461 8.196h-86.26q-22.087 0-37.544-15.457-15.457-15.457-15.457-37.544Zm482.696-380.608L785-832.652l48.652 48.087Zm-388 338.913h48.087l226.913-226.913-23.835-24.044-24.687-24.043-226.478 225.913v49.087Zm251.165-250.957-24.687-24.043 24.687 24.043 23.835 24.044-23.835-24.044Z'/></svg></button></td>".
+                                "</form>".
+                                "<form action='deleteInterface.php' method='GET'>".
+                                    "<td class='align-middle text-center' style='margin: 0 !important; width: 38px;'><button type='submit' name='id' value='$id' class='btn btn-danger' style='padding: 0 !important; height: 38px; width: 38px; display: flex; align-items: center; justify-content: center;'><svg xmlns='http://www.w3.org/2000/svg' fill='white' height='20' viewBox='0 -960 960 960' width='20'><path d='M277.37-111.869q-37.783 0-64.392-26.609-26.609-26.609-26.609-64.392v-514.5q-19.152 0-32.326-13.173-13.174-13.174-13.174-32.327 0-19.152 13.174-32.326t32.326-13.174H354.5q0-19.152 13.174-32.326T400-853.87h159.522q19.152 0 32.326 13.174t13.174 32.326h168.609q19.152 0 32.326 13.174t13.174 32.326q0 19.153-13.174 32.327-13.174 13.173-32.326 13.173v514.5q0 37.783-26.609 64.392-26.609 26.609-64.392 26.609H277.37Zm78.326-168.37h85.5v-360h-85.5v360Zm163.108 0h85.5v-360h-85.5v360Z'/></svg></button></td>".
                                 "</form>".
                             "</tr>";
                         }
@@ -50,16 +53,14 @@
                     }
                     ?>
             </table>
+            <h5>Showing <code style="background-color: #c9c9c9; border-radius: 3px; padding-inline: 2px; color: #383838; font-weight: 600;"><?php echo mysqli_num_rows($result); ?></code> Records</h5>
+            <hr>
         </div>
-        <div class="container">
-
-        </div>
-
 
         <div class="container">
             <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addModal">
-                Add New Records
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addModal" style="display: flex; justify-content: center; align-items: center; padding: 6px 12px; gap: 4px">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="white" height="20" viewBox="0 -960 960 960" width="20"><path d="M417-417H229q-26 0-44.5-18.5T166-480q0-26 18.5-44.5T229-543h188v-188q0-26 18.5-44.5T480-794q26 0 44.5 18.5T543-731v188h188q26 0 44.5 18.5T794-480q0 26-18.5 44.5T731-417H543v188q0 26-18.5 44.5T480-166q-26 0-44.5-18.5T417-229v-188Z"/></svg>Add New Records
             </button>
 
             <!-- Modal -->
